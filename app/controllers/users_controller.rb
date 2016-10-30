af9b17f9bf803a24a6992580511e62710ea4331b
Thaopt191 <thaopt191@gmail.com>
2016-10-30 16:57:16 +0700

    basic-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # log_in @user
-      flash[:success] = "Create successfully!"
+      log_in @user
+      flash[:success] = t("user.creatSuccess")
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+
   root "static_pages#home"
 
   get "users/show"
@@ -9,8 +11,12 @@ Rails.application.routes.draw do
 
   post "/signup", to: "users#create"
 
-  get "static_pages/home"
-
+  get  "/login", to: "sessions#new"
+  
+  post  "/login", to: "sessions#create"
+  
+  delete  "/logout", to: "sessions#destroy"
+  
   resources :users
   
   end
