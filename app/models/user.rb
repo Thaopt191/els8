091292944d6dd929e2bf029096b78fbd8f94c821
Thaopt191 <thaopt191@gmail.com>
2016-11-13 07:46:47 +0700

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
   has_many :activities
   has_many :results
+  validates :name, presence: true, length: {maximum: Settings.name_max_length}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email, presence: true, length: {maximum: Settings.email_max_length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  
+  has_secure_password
+  validates :password, presence: true, length: {minimum: Settings.pw_min_length}
+ 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
